e2733c64d56c0f39367d330aaf5b3108e2ee0b03
Byron Kruger <bkruger@jhb.dvt.co.za>
2022-09-03 17:26:07 +0200

    Commit made from VS Code Azure Static Web Apps

--- src/app/services/musicArtist/music-artist-details.service.ts
+++ src/app/services/musicArtist/music-artist-details.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 
@@ -11,7 +11,20 @@ export class MusicArtistDetailsService {
   constructor(private http: HttpClient) { }
 
   getArtistDetail(): Observable<any> {
-    // return this.http.get('https://api.deezer.com/artist/13');
-    return this.http.get('https://catfact.ninja/fact');
+    // this.http.jsonp('https://api.deezer.com/artist/13?output=jsonp', 'JSONP_CALLBACK').subscribe((a) => {
+    //   console.log(a)
+    // });
+    // this.http.get('https://catfact.ninja/fact').subscribe((fact: any) => {
+    //   console.log(fact.fact)
+    // });
+    return this.http.jsonp('https://api.deezer.com/artist/13?output=jsonp&callback=ng_jsonp_callback_0', 'JSONP_CALLBACK');
+    // const headers = new HttpHeaders();
+    // headers.set("credentials", "include");
+    // return this.http.get('https://api.deezer.com/artist/13?output=json', { headers: headers });
   }
+
+  // ng_jsonp_callback_0(a: any) {
+  // console.log("eminem");
+  // console.log(a);
+  // }
 }
