8408829d6d21f0ce34a4d18eb376db1c475be349
Byron Kruger <bkruger@jhb.dvt.co.za>
2022-09-04 19:39:45 +0200

    Added cat facts

--- src/app/services/musicArtist/music-artist-details.service.ts
+++ src/app/services/musicArtist/music-artist-details.service.ts
@@ -11,13 +11,17 @@ export class MusicArtistDetailsService {
   constructor(private http: HttpClient) { }
 
   getArtistDetail(): Observable<any> {
+    // return this.http.get('https://api.deezer.com/artist/13');
     // this.http.jsonp('https://api.deezer.com/artist/13?output=jsonp', 'JSONP_CALLBACK').subscribe((a) => {
     //   console.log(a)
     // });
-    // this.http.get('https://catfact.ninja/fact').subscribe((fact: any) => {
+    return this.http.get('https://catfact.ninja/fact');
+    // .subscribe((fact: any) => {
     //   console.log(fact.fact)
     // });
-    return this.http.jsonp('https://api.deezer.com/artist/13?output=jsonp&callback=ng_jsonp_callback_0', 'JSONP_CALLBACK');
+
+    // return this.http.jsonp('https://api.deezer.com/artist/13?output=jsonp&callback=ng_jsonp_callback_0', 'JSONP_CALLBACK');
+
     // const headers = new HttpHeaders();
     // headers.set("credentials", "include");
     // return this.http.get('https://api.deezer.com/artist/13?output=json', { headers: headers });
